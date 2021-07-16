f4c9ef3726c3f04733e3f99bfdda216c065073f7
Johnny Dreher <johnnydreher@gmail.com>
2021-07-16 17:37:00 -0300

    Encoders funcionando com medições de 6m

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -11,54 +11,84 @@
 #include <frc2/command/SubsystemBase.h>
 #include "ctre/Phoenix.h"
 #include <frc/SpeedControllerGroup.h>
-
+#include <frc/Encoder.h>
+#include <frc/Joystick.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <wpi/math>
+#include <frc/DutyCycleEncoder.h>
 class Robot : public frc::TimedRobot
 {
 public:
   double rAccel = 0, lAccel = 0;
+  double accel = -1;
   Robot()
   {
     m_robotDrive.SetExpiration(0.1);
     m_timer.Start();
+    m_encoderLeft.SetDistancePerPulse(48.0 / 2048.0);
+    m_encoderRight.SetDistancePerPulse(48.0 / 2048.0);
+   
   }
+  
 
   void AutonomousInit() override
   {
     m_timer.Reset();
     m_timer.Start();
+    m_robotDrive.TankDrive(accel,accel);
+    m_encoderLeft.Reset();
+    m_encoderRight.Reset();
+    accel = -0.5;
   }
 
   void AutonomousPeriodic() override
   {
-    // Drive for 2 seconds
-    if (m_timer.Get() < 2.0)
-    {
-      // Drive forwards half speed
-      m_robotDrive.ArcadeDrive(0.5, 0.0);
-    }
-    else
-    {
-      // Stop robot
-      m_robotDrive.ArcadeDrive(0.0, 0.0);
-    }
+     frc::SmartDashboard::PutNumber("Encoder Esquerda", m_encoderLeft.GetDistance());
+     frc::SmartDashboard::PutNumber("Encoder Direita", m_encoderRight.GetDistance());
+     frc::SmartDashboard::PutNumber("Aceleração", accel);
+
+     if (m_encoderLeft.GetDistance()>590.0 || m_encoderRight.GetDistance()>590.0)
+     {
+       accel = 0;
+     }
+     else if (m_encoderLeft.GetDistance()>580.0 || m_encoderRight.GetDistance()>580.0)
+     {
+        accel = 0.25;
+     }
+     else if (m_encoderLeft.GetDistance()>500.0 || m_encoderRight.GetDistance()>500.0)
+     {
+        accel = -0.5;
+     }
+     else if (m_encoderLeft.GetDistance()>100.0 || m_encoderRight.GetDistance()>100.0)
+     {
+       accel = -1.0;
+     }
+     
+    
+     m_robotDrive.TankDrive(accel,accel);
+
   }
 
   void TeleopInit() override {
     lAccel = 0;
     rAccel = 0;
+     m_encoderLeft.Reset();
+       m_encoderRight.Reset();
   }
 
   void TeleopPeriodic() override
   {
-    lAccel = ((lAccel*99)+m_stick.GetY(lHand))/100;
-    rAccel = ((rAccel*9)+m_stick.GetX(rHand))/10;
-    if(m_stick.GetStickButton(lHand) || m_stick.GetStickButton(rHand) ||
-        m_stick.GetAButton() || m_stick.GetBButton()){
-      lAccel = 0;
-      rAccel = 0;
+     frc::SmartDashboard::PutNumber("Encoder Esquerda", m_encoderLeft.GetDistance());
+     frc::SmartDashboard::PutNumber("Encoder Direita", m_encoderRight.GetDistance());
+    lAccel = m_stick.GetY(lHand);
+    rAccel = m_stick.GetX(lHand);
+    if(m_stick.GetXButtonPressed())
+    {
+     m_encoderLeft.Reset();
+       m_encoderRight.Reset();  
     }
 
-    m_robotDrive.ArcadeDrive(lAccel, rAccel);
+    m_robotDrive.ArcadeDrive(lAccel,rAccel);
 
   }
 
@@ -68,10 +98,10 @@ public:
 
 private:
   // Robot drive system
-  WPI_VictorSPX m_frontLeft{0};
-  WPI_VictorSPX m_frontRight{1};
-  WPI_VictorSPX m_rearLeft{2};
-  WPI_VictorSPX m_rearRight{3};
+  WPI_VictorSPX m_frontLeft{4};
+  WPI_VictorSPX m_frontRight{5};
+  WPI_VictorSPX m_rearLeft{6};
+  WPI_VictorSPX m_rearRight{7};
   frc::SpeedControllerGroup m_left{m_frontLeft, m_rearLeft};
   frc::SpeedControllerGroup m_right{m_frontRight, m_rearRight};
   frc::DifferentialDrive m_robotDrive{m_right,m_left};
@@ -82,6 +112,22 @@ private:
   frc::XboxController m_stick{0};
   frc::LiveWindow &m_lw = *frc::LiveWindow::GetInstance();
   frc::Timer m_timer;
+  //encoder esquerda
+  // 9 - enc I
+  // 8 - enc B
+  // 7 - enc A
+  // 6 - enc ABS
+  //encoder direita
+  // 5 - enc I
+  // 4 - enc B
+  // 3 - enc A
+  // 2 - enc ABS
+  
+  frc::Encoder m_encoderLeft{8, 7};
+  frc::Encoder m_encoderRight{4, 3};
+
+
+
 };
 
 #ifndef RUNNING_FRC_TESTS
